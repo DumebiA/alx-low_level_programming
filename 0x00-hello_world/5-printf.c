d08208e7037720f620ff195fec1d10db2b1c2fe4
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-03-16 13:12:22 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - acts as main function
+ *
+ * Return: program ends with 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
