0b477ea3ffe60d9090379513a748f79c3ab9b5e5
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-03-16 18:25:04 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - printing program
+ *
+ * Return: Success returns 1
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
