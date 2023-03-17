f0948a3191bb259d714b84d574ff3c8cfcbb1169
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-03-17 16:59:57 -0700

    print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,24 @@
 int main(void)
 {
 	int n;
+	int o;
 
 	for (n = 0 ; n < 10 ; n++)
-		putchar(n);
+	{
+		for (o = 1 ; o < 10 ; o++)
+		{
+			if (n < o && n != o)
+			{
+				putchar(n + '0');
+				putchar(o + '0');
+				if (n + o != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
