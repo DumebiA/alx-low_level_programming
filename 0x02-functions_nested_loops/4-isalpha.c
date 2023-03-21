d6e4f81d5f964d49a3ff09ae430ec6d8f95f5455
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-03-21 16:27:21 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isalpha - main function
+ *
+ * @c: to check c
+ *
+ * Return: returns to 1 if alpabet
+ * and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
