8c085cb5751955299a97b7e432d06faa261ba949
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-03-27 12:49:11 -0700

    03

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints string, a new line, then stdout
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+		_putchar('\n');
+}
