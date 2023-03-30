d323e956edcbea77ff21e5792ae26433621fa06b
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-03-30 09:34:54 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two string
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int o;
+	int m;
+
+	o = 0;
+	while (dest[o] != '\0')
+	{
+		o++;
+	}
+	m = 0;
+	while (m < n && src[m] != '\0')
+	{
+	dest[o] = src[m];
+	o++;
+	m++;
+	}
+	dest[o] = '\0';
+	return (dest);
+}
