fa9a43dd3a5664c83156a87b06f37974d205ae95
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-04-03 12:44:40 -0700

    0x07

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - set value of a pointer
+ * @s: pointer in a pointer
+ * @to: pointer character
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
