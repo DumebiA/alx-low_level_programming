6649dfb6eb069dc02f2e515972a95b23af045d91
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-05-01 06:45:31 -0700

    task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -13,11 +13,11 @@ size_t listint_len(const listint_t *h)
 {
 	size_t value = 0;
 
-	while (h)
-		{
-			value++;
-			h = h->next;
-		}
+while (h)
+{
+	value++;
+	h = h->next;
+}
 
-	return (value);
+return (value);
 }
