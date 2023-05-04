39c68b6589306d7a57abf1fe0e174e4f5580576f
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-05-04 11:18:40 -0700

    a

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,22 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned i;
-	for (i = 1 << 31; i > 0; i = i / 2)
-		(n & i) ? printf("1") : printf("0");
+	int m;
+	int sum = 0;
+	unsigned long int pres;
+
+	for (m = 89; m >= 0; m--)
+	{
+		pres = n >> m;
+
+		if (pres & 1)
+		{
+			_putchar('1');
+			sum++;
+		}
+		else if (sum)
+			_putchar('0');
+	}
+	if (!sum)
+		_putchar('0');
 }
